7665136549f6cf400f3a1c03a4e2eba0214b6128
Jim Grimes <jimgrimes86@gmail.com>
2023-12-26 14:04:36 -0500

    Completed reverse-a-string

--- 01-week-1--starter-algorithms/00-day-1--reverse-a-string/javascript/reverse_string.js
+++ 01-week-1--starter-algorithms/00-day-1--reverse-a-string/javascript/reverse_string.js
@@ -1,5 +1,10 @@
 function reverseString(str) {
   // type your code here
+  let newString = "";
+  for (let i=0; i<str.length; i++) {
+    newString = str[i]+newString
+  }
+  return newString
 }
 
 if (require.main === module) {
@@ -11,9 +16,27 @@ if (require.main === module) {
 
   console.log("Expecting: 'ybabtac'");
   console.log("=>", reverseString("catbaby"));
+
+  console.log("Expecting: 'olleh'")
+  console.log("=>", reverseString("hello"))
 }
 
 module.exports = reverseString;
 
 // Please add your pseudocode to this file
 // And a written explanation of your solution
+
+/*
+start: hello
+i=0: elloh
+i=1: lloeh
+i=2: loleh
+i=3: olleh
+*/
+
+/*
+- declare new variaible newString
+- iterate through str using a counter i
+- for each element in str, add to beginning of newString
+- return newString
+*/
